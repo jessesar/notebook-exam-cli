3a13f6fcb065281b12f66e6487be593265855657
Jesse van der Sar <jesse@sar.nl>
2017-12-21 13:52:08 +0100

    Fix install

--- install.sh
+++ install.sh
@@ -22,8 +22,8 @@ fi
 
 if [ -f ~/anaconda/envs/python3/bin/pip ]; then
     echo "Installing questions module (Python 3)..."
-    pip3 uninstall -y questions 1> /dev/null
-    pip3 install https://github.com/jessesar/uva-questions/archive/master.zip 1> /dev/null
+    ~/anaconda/envs/python3/bin/pip uninstall -y questions 1> /dev/null
+    ~/anaconda/envs/python3/bin/pip install https://github.com/jessesar/uva-questions/archive/master.zip 1> /dev/null
 fi
 
 bold=$(tput bold)
